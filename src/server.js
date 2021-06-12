d26e34f9bedb53b1e1c023286a73b4e5a9701c11
Moscode <odunoshomoses@gmail.com>
2021-06-12 20:50:22 +0100

    More code are added to server.js file

--- src/server.js
+++ src/server.js
@@ -2,3 +2,6 @@ const express = require("express");
 const jsonServer = require("json-server");
 const chokidar = require("chokidar");
 const cors =require("cors");
+
+const fileName = process.argv[2] || "./data.js"
+const port = process
