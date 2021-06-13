451637af073f29147b0c7aede146f321fbea6ce5
Moscode <odunoshomoses@gmail.com>
2021-06-13 10:28:36 +0100

    Server setup is going on

--- src/server.js
+++ src/server.js
@@ -4,4 +4,21 @@ const chokidar = require("chokidar");
 const cors =require("cors");
 
 const fileName = process.argv[2] || "./data.js"
-const port = process
+const port = process.argv[3] || 3500;
+
+let router = undefined;
+
+const app = express();
+
+const createServer = () => {
+    delete require.cache[require.resolve(fileName)];
+    setTimeout(() => {
+        router = jsonServer.router(fileName.endsWith(".js")
+        ? require(fileName)() : fileName);
+    }, 100)
+}
+
+createServer();
+app.use(cors());
+app.use(jsonServer.bodyparser);
+app.use("")
