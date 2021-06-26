80e13209fb1d133420f3d53c6b13b674df7a9cd9
Moscode <odunoshomoses@gmail.com>
2021-06-26 21:02:30 +0100

    ProductList file added

---
+++ src/shop/ProductList.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export class ProductList extends Component {
+    
+    render() {
+        if (this.props.products = || this.props.products.length == 0){
+            
+        }
+    }
+}
