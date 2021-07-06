c27cc8527b0937275d32e85db1230da07abf6a50
Moscode <odunoshomoses@gmail.com>
2021-07-06 17:39:59 +0100

    Yeah

--- src/shop/ProductList.js
+++ src/shop/ProductList.js
@@ -3,8 +3,9 @@ import React, { Component } from "react";
 export class ProductList extends Component {
     
     render() {
-        if (this.props.products = || this.props.products.length == 0){
-            
+        if (this.props.products == null || this.props.products.length === 0){
+            return <h5 className="p-2">No products</h5>
         }
+        return this.props.products.map
     }
 }
